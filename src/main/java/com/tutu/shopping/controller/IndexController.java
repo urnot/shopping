bca307a6495cf9837b77899bebf1100b8a22c8f9
scarlett <jialishen7@gmail.com>
2018-08-30 14:46:42 +0800

    增加拦截

--- src/main/java/com/tutu/shopping/controller/IndexController.java
+++ src/main/java/com/tutu/shopping/controller/IndexController.java
@@ -4,9 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping
 public class IndexController {
 	
-	@RequestMapping(value= {"/welcome","/index"})
+	@RequestMapping("/")
 	public String toIndex() {
 		return "index";
 	}
---
+++ src/main/java/com/tutu/shopping/controller/PhotoController.java
@@ -0,0 +1,9 @@
+package com.tutu.shopping.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller("/photo")
+public class PhotoController {
+	
+
+}
